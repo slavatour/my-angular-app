8bf6bdf71ed14db9f97d2a45fd415e9125b585d8
yonchul <yonch@softserveinc.com>
2015-03-26 19:02:07 +0200

    added validation for 'name' and 'surname' failds

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -14,6 +14,7 @@ controllers.controller('PhoneController', ['$scope', '$routeParams', function($s
 }]);
 
 controllers.controller('CartController', ['$scope', function($scope){
+    $scope.continents = [{id:1, continent:'america'},{id:2, continent:'africa'},{id:2, continent:'europe'}]
 
     $scope.submitCart = function(){
       console.log("submit", $scope.order);
